e548a084f2207023c68263e556387a90f46457aa
David Eliason <davethemaker@gmail.com>
2018-09-10 17:34:14 -0700

    craete route for response object with sample data for front-end to render

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Wow this is via Github much better!')
 })
+
+app.get('/api', (req,res) => {
+	res.json([{
+		"name" : "Tom",
+		"fave_color" : "blue"
+	},
+		{
+			"name" : "David",
+			"fave_color" : "green"
+		}])
+});
 app.listen(3000, () => console.log('Server running on port 3000'))
